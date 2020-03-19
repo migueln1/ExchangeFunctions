4e5ac19a893e98e4b2d095ac185946e69417e3e1
Miguelangel Nuñez <migueln1@outlook.com>
2020-03-18 20:25:20 -0400

    Update FetchRates.js

--- Functions/FetchRates.js
+++ Functions/FetchRates.js
@@ -1,11 +1,15 @@
 exports.handler = async event => {
   event = event || {};
-  const id = event.queryStringParameters.id || "World";
-  let rates = getRatesAsync(id)
-  return {
-    statusCode: 200,
-    body: JSON.stringify(rates)
-  };
+  const id = event.queryStringParameters.id || "ARS";
+  getRatesAsync(id).then(data => {
+      console.log(data)
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+      };
+    })
+    .catch(err=>console.log(err))
+ 
 }
 async function getRatesAsync(id) 
 {
