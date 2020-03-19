9b65c573427e91433e1017e3e826108d8a6276dc
miguel <migueln1@outlook.com>
2020-03-19 08:45:10 -0400

    import

--- Functions/FetchRates.js
+++ Functions/FetchRates.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch')
+import fetch from 'node-fetch'
 exports.handler = async event => {
   event = event || {};
   const id = event.queryStringParameters.id || "ARS";
